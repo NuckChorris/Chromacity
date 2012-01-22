91399f31cf875cf4d28aa845f0d5f875dc482cc6
Peter Lejeck <peter.lejeck@gmail.com>
2012-01-22 02:57:25 -0800

    Change version to match beta versions, add icon

--- cwVer.js
+++ cwVer.js
@@ -1,6 +1,7 @@
 cwVer.callbacks['f95c642df9866494e52cc7bccbd21491']({
 	'name': 'Chromacity',
-	'version': '2.0.0',
+	'version': '2.0.0b2',
+	'icon': 'http://i39.tinypic.com/2en92ee.png',
 	'downloads': {
 		'chrome': 'https://github.com/NuckChorris/Chromacity/raw/master/chromacity.user.js',
 		'firefox': 'https://github.com/NuckChorris/Chromacity/raw/master/chromacity.user.js',
