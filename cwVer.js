d796549ea1c77c426b3e85e96952caa4c491cc13
Peter Lejeck <peter.lejeck@gmail.com>
2012-01-22 04:00:13 -0800

    Update to Beta 3

--- cwVer.js
+++ cwVer.js
@@ -1,6 +1,6 @@
 cwVer.callbacks['f95c642df9866494e52cc7bccbd21491']({
 	'name': 'Chromacity',
-	'version': '2.0.0b2',
+	'version': '2.0.0b3',
 	'icon': 'http://i39.tinypic.com/2en92ee.png',
 	'downloads': {
 		'chrome': 'https://github.com/NuckChorris/Chromacity/raw/master/chromacity.user.js',
