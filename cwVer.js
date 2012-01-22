ff4dc0b0ecdf3f0c4e6a7a6cd3b333769b1b1d7a
Peter Lejeck <peter.lejeck@gmail.com>
2012-01-22 00:06:28 -0800

    Create versioning info for Chromacity
    * moving from personal gh-pages

---
+++ cwVer.js
@@ -0,0 +1,9 @@
+cwVer.callbacks['aa11d9431dd3c512b4b8c5a5c39e2922']({
+	'name': 'Chromacity',
+	'version': '2.0.0',
+	'downloads': {
+		'chrome': 'https://github.com/NuckChorris/Chromacity/raw/master/chromacity.user.js',
+		'firefox': 'https://github.com/NuckChorris/Chromacity/raw/master/chromacity.user.js',
+		'safari': 'https://github.com/NuckChorris/Chromacity/raw/master/chromacity.user.js'
+	}
+});
